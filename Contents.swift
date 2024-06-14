d354f399ad5bb49e039b88d5305cad61834d4b59
aishuraja <aishuraja2001@gmail.com>
2024-06-14 10:25:21 -0400

    iOS Playground developing iOS applications

---
+++ Contents.swift
@@ -0,0 +1,25 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+var fname = "Aishwarya"
+let lname = "Raja"
+let quote = "All is well"
+let mqoute = """
+A day
+in
+my life
+"""
+print(fname.count)
+print(quote.hasPrefix("A day"))
+print(mqoute.hasSuffix("my life"))
+
+var score = 10
+score+=2
+print(score)
+print(score.isMultiple(of:6 ))
+
+
+
+let a = 0.1
+let b = 0.2
+print(a+b)
