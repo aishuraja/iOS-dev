b2d37605ec109248d708048df0acec41a4ca20ad
aishuraja <aishuraja2001@gmail.com>
2024-06-17 15:03:57 -0400

    arrays

--- Contents.swift
+++ Contents.swift
@@ -1,25 +1,101 @@
 import Cocoa
 
-var greeting = "Hello, playground"
-var fname = "Aishwarya"
-let lname = "Raja"
-let quote = "All is well"
-let mqoute = """
-A day
-in
-my life
-"""
-print(fname.count)
-print(quote.hasPrefix("A day"))
-print(mqoute.hasSuffix("my life"))
+//var greeting = "Hello, playground"
+//var fname = "Aishwarya"
+//let lname = "Raja"
+//let quote = "All is well"
+//let mqoute = """
+//A day
+//in
+//my life
+//"""
+//print(fname.count)
+//print(quote.hasPrefix("A day"))
+//print(mqoute.hasSuffix("my life"))
+//
+//var score = 10
+//score+=2
+//print(score)
+//print(score.isMultiple(of:6 ))
+//
+//
+//
+//let a = 0.1
+//let b = 0.2
+//print(a+b)
 
-var score = 10
-score+=2
-print(score)
-print(score.isMultiple(of:6 ))
+var boo1 = true
+var boo2 = false
+//using ! operator
+print(boo1)
+boo1 = !boo1
+print(boo1)
+//using toggle()
+print("value of boo2:", boo2)
+boo2.toggle()
+print("value of boo2 using toggle:",boo2)
 
 
+//string concadenation
+let statement1 = "I have"
+let statement2 = 5
+let statement3 = "chocolates"
+let result = statement1 + " " + String(statement2) + " " + statement3
+print(result)
 
-let a = 0.1
-let b = 0.2
-print(a+b)
+//To optimize use string interpolation
+let result1 = "\(statement1) \(statement2) \(statement3)"
+print(result1)
+//another example:
+
+let example = 84.7
+print("My current weight is \(example) ")
+
+//converting temperature from C to F
+let tempc = 55.7
+var tempf = ((tempc*9)/5) + 32
+print("celius: \(tempc)°C Farhentient: \(tempf)°F")
+
+// arrays
+// try with append function
+var names = ["Aishu", "John" ,"mathew"]
+names.append("Hosanna")
+print(names)
+names.count
+names.remove(at: 2)
+names.count
+print(names.contains("Aishu"))
+print(names.sorted())
+print(names.reversed())
+ 
+// dictionaries
+let employees = [
+    "name": "Aishu",
+    "job title" : "Software Engineer",
+    "location" : "San Fransico"
+]
+print(employees["name", default: "unknown"] )
+print(employees["job title", default: "unknown"])
+// creating an empty dictinoary and set the key value
+var marks = [String:Int]()
+marks["Ishh"] = 100
+marks["Kali"] = 80
+marks["don"] = 50
+marks["kuns"] = 20
+print("the marks of students:",marks)
+
+//sets
+var actors = Set<String>()
+actors.insert("Tom cruise")
+actors.insert("Danzel Wahington")
+actors.insert("Nicholas Cage")
+actors.insert("Jacky chan")
+print(actors)
+
+//enum
+enum weekday {
+    case Monday, Tuesday, Wednesday, Thursday, Friday
+}
+var days = weekday.Monday
+days = .Friday
+days = .Thursday
--- README.md
+++
@@ -1,4 +0,0 @@
-# iOS-dev
-
-**It's always exciting to see the new features in every iOS update, but this time I've decided to dive deeper and learn how to develop applications myself. Being an iOS enthusiast is incredibly fun and rewarding. Though I've just begun my journey, I'm confident that I'll cherish this experience and create some amazing projects to share with you all. Stay tuned for updates!**
-
