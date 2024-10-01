de60db84d3969ad22bb715c158d4477ff6943821
aishuraja <aishuraja2001@gmail.com>
2024-10-01 11:55:46 -0400

    few more changes

---
+++ Jubliee_old/Jubliee/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Jubliee
+//
+//  Created by Aishwarya Raja on 9/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Jubliee_old/Jubliee/JublieeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JublieeApp.swift
+//  Jubliee
+//
+//  Created by Aishwarya Raja on 9/20/24.
+//
+
+import SwiftUI
+
+@main
+struct JublieeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
