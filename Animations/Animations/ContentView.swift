20cde51c3e659942a18ebee6233b18fe4c1f443d
aishuraja <aishuraja2001@gmail.com>
2024-12-19 16:46:20 -0500

    Animation

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -11,15 +11,38 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     var body: some View {
         Button("Tap me"){
-            animationAmount += 1
+//            animationAmount += 1
         }
         .padding(50)
         .background(.red)
         .foregroundColor(.white)
         .clipShape(.circle)
-        .blur(radius: (animationAmount - 1) * 3)
-        .scaleEffect(animationAmount)
-        .animation(.default , value: animationAmount)
+        .overlay(
+            Circle()
+                .stroke(.red)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(
+                    .easeInOut(duration: 1)
+                    .repeatForever(autoreverses: false),
+                    value: animationAmount
+                
+                )
+        
+        
+            
+        )
+        .onAppear{
+            animationAmount = 2
+        }
+//        .blur(radius: (animationAmount - 1) * 3)
+//        .scaleEffect(animationAmount)
+//        .animation(
+//            .easeInOut(duration: 2)
+//            /*.delay(1),*/ // delay waits for 1 second before executing 2 sec animation
+//                .repeatCount(4, autoreverses: false),
+//        value: animationAmount
+//        )
     }
 }
 
