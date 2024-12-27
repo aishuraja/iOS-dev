374db4345885da1665f4471c7824ba7d358a3351
aishuraja <aishuraja2001@gmail.com>
2024-12-27 13:08:04 -0500

    Expense Tracker

---
+++ PocketPlan/PocketPlan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PocketPlan
+//
+//  Created by Aishwarya Raja on 12/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PocketPlan/PocketPlan/PocketPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketPlanApp.swift
+//  PocketPlan
+//
+//  Created by Aishwarya Raja on 12/25/24.
+//
+
+import SwiftUI
+
+@main
+struct PocketPlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
