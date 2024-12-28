1ffa96c434ebf6d2f77d998258f2ca929650e12e
aishuraja <aishuraja2001@gmail.com>
2024-12-28 14:37:29 -0500

    Expense Tracker

--- PocketPlan/PocketPlan/ContentView.swift
+++ PocketPlan/PocketPlan/ContentView.swift
@@ -7,15 +7,42 @@
 
 import SwiftUI
 
+struct Expenses : Identifiable {
+    let id = UUID()
+    let name: String
+    let amount : Double
+    let type: String
+    
+}
+
+@Observable
+class ExpenseTrack {
+    var items = [Expenses]() // Array of expenses
+    
+}
+
 struct ContentView: View {
+    @State private var exp = ExpenseTrack()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            List{
+                ForEach(exp.items){ item in
+                    Text(item.name)
+                    
+                }.onDelete(perform: removeItems)
+            }.navigationTitle("Pocket-Plan")
+                .toolbar{
+                    Button("Add item", systemImage: "plus"){
+                        let addedExpense = Expenses(name: "Food", amount: 5, type: "Personal" )
+                        exp.items.append(addedExpense)
+                    }
+                    
+                }
+            
         }
-        .padding()
+    }
+    func removeItems(at offsets: IndexSet){
+        exp.items.remove(atOffsets: offsets)
     }
 }
 
