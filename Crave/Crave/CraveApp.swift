344c9fecb9a3e0b17653953fb2e4bad5eed21e26
aishuraja <aishuraja2001@gmail.com>
2025-01-10 23:28:30 -0500

    Recipe application

---
+++ Crave/Crave/CraveApp.swift
@@ -0,0 +1,17 @@
+//
+//  CraveApp.swift
+//  Crave
+//
+//  Created by Aishwarya Raja on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct CraveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
